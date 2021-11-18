7178ac238563faca8ac4ff3bcc41866fcd41765b
vane12ssa <93740390+vane12ssa@users.noreply.github.com>
2021-11-18 14:25:37 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,5 +2,10 @@ var check=document.querySelector(".check");
 check.addEventListener('click',idioma);
 
 function idioma(){
-  console.log(check.checked);
+  let id=check.checked;
+  if(id==true{
+     location.href="../en/index.html";
+  }else{
+    location.href="../index.html";
+  }
 }
