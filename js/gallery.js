573253820d31134365943c643d32868d76a6f716
vane12ssa <93740390+vane12ssa@users.noreply.github.com>
2021-11-19 00:29:04 -0600

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -34,21 +34,3 @@
     dots[slideIndex-1].className += " active";
     captionText.innerHTML = dots[slideIndex-1].alt;
   }
-
-  function showSlides1(n) {
-    var i;
-    var slides1 = document.getElementsByClassName("mySlides1");
-    var dots1 = document.getElementsByClassName("demo1");
-    var captionText1 = document.getElementById("caption1");
-    if (n > slides1.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides1.length}
-    for (i = 0; i < slides1.length; i++) {
-        slides1[i].style.display = "none";
-    }
-    for (i = 0; i < dots1.length; i++) {
-        dots1[i].className = dots1[i].className.replace(" active", "");
-    }
-    slides1[slideIndex-1].style.display = "block";
-    dots1[slideIndex-1].className += " active";
-    captionText1.innerHTML = dots1[slideIndex-1].alt;
-  }
