5f16c821dc1e8f13febcbb1ecc74aa384ad1d7c0
vane12ssa <93740390+vane12ssa@users.noreply.github.com>
2021-11-18 20:45:42 -0600

    Update libros.js

--- js/libros.js
+++ js/libros.js
@@ -1,11 +1,10 @@
 var check=document.querySelector(".check");
 check.addEventListener('click',idioma);
-
 function idioma(){
   let id=check.checked;
   if (id==true){
-     location.href="../paginas/libros.html";
+     location.href="../en/libros.html";
   }else{
-    location.href="../en/Libros.html";
+    location.href="../paginas/Libros.html";
   }
 }
